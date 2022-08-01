57e12b6ac4cf78118b5651408bb0c41b064a42a8
kodapassey <kodapassey@charter.net>
2022-07-31 18:36:10 -0700

    completed productTag.js columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,20 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
